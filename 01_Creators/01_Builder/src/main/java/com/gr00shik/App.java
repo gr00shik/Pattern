4da4f9c240bb9b160635d343b4653548e1e9827e
sergey <test@test.com>
2019-03-26 23:04:32 +0300

    gr00shik-000 initial commit

---
+++ 01_Creators/01_Builder/src/main/java/com/gr00shik/App.java
@@ -0,0 +1,13 @@
+package com.gr00shik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+
+        System.out.println("Start project to demonstrate Builder pattern");
+
+    }
+}
